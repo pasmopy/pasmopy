72170c9831f4e0e62af0d05d779088ccba3f6a85
himoto <31299606+himoto@users.noreply.github.com>
2021-02-02 10:16:09 +0900

    Update codes

--- dyaus/patient_specific_model.py
+++ dyaus/patient_specific_model.py
@@ -1,32 +1,32 @@
-import sys
+import os
 from dataclasses import dataclass
+from importlib import import_module
 from typing import List, Optional
 
 import biomass
+from tqdm import tqdm
 
 
 @dataclass
 class PatientSpecificModel(object):
     patients: List[str]
 
-    def run(self, options: Optional[dict] = None):
+    def run(self, path_to_models: str, options: Optional[dict] = None):
         if options is None:
             options = {}
         options.setdefault("viz_type", "average")
         options.setdefault("show_all", False)
         options.setdefault("stdev", True)
         options.setdefault("save_format", None)
-        for idx, patient in enumerate(self.patients):
-            exec(f"from models import {patient.strip()}")
-            exec(f"model = {patient.strip()}.create()")
-            biomass.run_simulation(model, **options)
-            sys.stdout.write(
-                "\r{} is done ({:d} / {:d}).\n".format(
-                    patient.strip(),
-                    idx,
-                    len(self.patients),
+        for patient in tqdm(self.patients):
+            biological_system = import_module(
+                os.path.join(
+                    path_to_models,
+                    patient,
                 )
             )
+            model = biological_system.create()
+            biomass.run_simulation(model, **options)
 
     def update_out(self, path_to_out: str):
         pass
