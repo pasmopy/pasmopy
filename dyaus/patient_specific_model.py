3cff28cb7265686bde7e4eed67421486984ef5b9
himoto <31299606+himoto@users.noreply.github.com>
2021-02-03 09:44:13 +0900

    Update dyaus source and tests

--- dyaus/patient_specific_model.py
+++ dyaus/patient_specific_model.py
@@ -1,6 +1,6 @@
+import multiprocessing
 import os
 from dataclasses import dataclass
-from importlib import import_module
 from typing import List, Optional
 
 import biomass
@@ -9,24 +9,48 @@ from tqdm import tqdm
 
 @dataclass
 class PatientSpecificModel(object):
+    """
+    Run simulations and analyses of patient-specific models.
+
+    Attributes
+    ----------
+    patients : list of strings
+        List of patients' names or indices.
+
+    """
+
     patients: List[str]
 
-    def run(self, path_to_models: str, options: Optional[dict] = None):
+    def run(
+        self,
+        path_to_models: str,
+        options: Optional[dict] = None,
+    ) -> None:
+        """
+        Run simulations of patient-specific models.
+
+        Parameters
+        ----------
+        path_to_models : str
+            Path to the directory containing patient-specific models.
+
+        options : dict, optional
+            Arguments of biomass.run_simulation.
+
+        """
         if options is None:
             options = {}
         options.setdefault("viz_type", "average")
         options.setdefault("show_all", False)
         options.setdefault("stdev", True)
-        options.setdefault("save_format", None)
+        options.setdefault("save_format", "pdf")
         for patient in tqdm(self.patients):
-            biological_system = import_module(
-                os.path.join(
-                    path_to_models,
-                    patient,
-                )
+            exec(
+                f"from {path_to_models.lstrip(f'.{os.sep}').replace(os.sep, '.')} "
+                f"import {patient}"
             )
-            model = biological_system.create()
-            biomass.run_simulation(model, **options)
+            exec(f"model = {patient}.create()")
+            exec("biomass.run_simulation(model, **options)")
 
     def update_out(self, path_to_out: str):
         pass
--- dyaus/tests/test_dyaus.py
+++ dyaus/tests/test_dyaus.py
@@ -1,2 +1,16 @@
-def test_dyaus():
+from ..patient_specific_model import PatientSpecificModel
+
+
+class BioMassExamples(PatientSpecificModel):
     pass
+
+
+def test_run_with_biomass_examples():
+    model_list = [
+        "circadian_clock",
+        "mapk_cascade",
+        "nfkb_pathway",
+        "tgfb_smad",
+    ]
+    biomass_examples = BioMassExamples(model_list)
+    assert biomass_examples.run("biomass.models", options={"viz_type": "original"}) is None
