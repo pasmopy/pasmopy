b991cb0460074f8380739dad521315c8a6427517
himoto <31299606+himoto@users.noreply.github.com>
2021-04-05 09:51:23 +0900

    Add insulin_signaling

--- tests/test_biomass_examples.py
+++ tests/test_biomass_examples.py
@@ -1,18 +1,19 @@
 from dyaus import PatientModelSimulations
 
 
-class BioMassExamples(PatientModelSimulations):
+class BiomassExamples(PatientModelSimulations):
     pass
 
 
-def test_run_with_biomass_examples():
+def test_biomass_examples():
     model_list = [
         "circadian_clock",
+        "insulin_signaling",
         "mapk_cascade",
         "nfkb_pathway",
         "tgfb_smad",
     ]
-    biomass_examples = BioMassExamples(
+    biomass_examples = BiomassExamples(
         "biomass.models",
         model_list,
         biomass_options={"viz_type": "original"},
