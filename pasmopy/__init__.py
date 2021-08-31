d94157ec2c43984246e99753db1aa82f9fa58d03
himoto <31299606+himoto@users.noreply.github.com>
2021-08-31 11:03:19 +0900

    Use importlib.metadata to read version

--- pasmopy/__init__.py
+++ pasmopy/__init__.py
@@ -2,7 +2,21 @@
 
 __author__ = __maintainer__ = "Hiroaki Imoto"
 __email__ = "himoto@protein.osaka-u.ac.jp"
-__version__ = "0.0.1"
+
+try:
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
+    del get_version
+except (LookupError, ImportError):
+    import sys
+    
+    if sys.version_info[:2] < (3, 8):
+        from importlib_metadata import version
+    else:
+        from importlib.metadata import version
+    __version__ = version(__name__)
+    del version
 
 
 from .construction import Text2Model
