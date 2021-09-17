0b4e1bb227a801bd26ee3645620832fef13fe295
Hiroaki Imoto <himoto@protein.osaka-u.ac.jp>
2021-09-17 11:26:34 +0100

    Revert "Bump version to 0.1.0"

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     __author__ = __maintainer__ = "Hiroaki Imoto"
     __email__ = "himoto@protein.osaka-u.ac.jp"
-    __version__ = "0.1.0"
+    __version__ = "0.0.3"
 
 
 
