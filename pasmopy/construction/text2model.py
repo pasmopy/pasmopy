855c7e2c135d7aceb7a4d87c023f5be615101a91
himoto <31299606+himoto@users.noreply.github.com>
2021-11-17 17:49:14 +0000

    Suppress FutureWarning raised by '-->' symbol in version 0.1.0

--- pasmopy/construction/text2model.py
+++ pasmopy/construction/text2model.py
@@ -1,6 +1,7 @@
 import os
 import re
 import shutil
+import warnings
 from dataclasses import dataclass, field
 from typing import List
 
@@ -12,6 +13,7 @@ except ImportError:
 from . import julia_template as jl
 from .reaction_rules import DuplicateError, ReactionRules
 
+warnings.simplefilter(action="ignore", category=FutureWarning)  # pasmopy==0.1.0
 
 @dataclass
 class Text2Model(ReactionRules):
