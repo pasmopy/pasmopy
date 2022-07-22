fb7b0c5fb4c473226cb3b00dd06f1d70d746b435
himoto <31299606+himoto@users.noreply.github.com>
2022-07-22 22:18:06 +0100

    Remove construction/

--- pasmopy/__init__.py
+++ pasmopy/__init__.py
@@ -1,9 +1,8 @@
 """Patient-Specific Modeling in Python"""
 
 from biomass.core import *
-from biomass.result import OptimizationResults
+from biomass.construction import Text2Model
 
-from .construction import Text2Model
 from .individualization import Individualization
 from .patient_model import PatientModelAnalyses, PatientModelSimulations
 from .version import __version__
--- pasmopy/construction/__init__.py
+++
@@ -1,1 +0,0 @@
-from .text2model import Text2Model, run_example_model
