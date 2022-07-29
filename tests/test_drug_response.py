46a5fa99c12a7e6a25ea3dde555f5af285f6f264
himoto <31299606+himoto@users.noreply.github.com>
2022-07-29 12:29:58 +0100

    Update tests

--- tests/test_drug_response.py
+++ tests/test_drug_response.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from urllib.error import URLError
 
 import pandas as pd
 from pasmopy.validation import CancerCellLineEncyclopedia
@@ -10,38 +11,43 @@ ErbB_expression_ratio: Final[pd.DataFrame] = pd.read_csv(
     "https://raw.githubusercontent.com/pasmopy/breast_cancer/master/drug_response/data/ErbB_expression_ratio.csv",
     index_col=0,
 )
-ccle: Final = CancerCellLineEncyclopedia()
+try:
+    ccle: Final = CancerCellLineEncyclopedia()
+except URLError:
+    ccle = None
 
-def test_dose_response_curve():
-    for drug in ["Erlotinib", "Lapatinib"]:
-        assert ccle.save_dose_response_curve(
-            ErbB_expression_ratio,
-            {"value": ["high", "low"]},
-            drug,
-            labels=["EGFR high", "EGFR low"],
-        ) is None
+if ccle is not None:
 
-    for drug in ["Erlotinib", "Lapatinib"]:
-        assert os.path.isfile(
-            os.path.join("dose_response", "EGFR", f"{drug}.pdf")
-        )
+    def test_dose_response_curve():
+        for drug in ["Erlotinib", "Lapatinib"]:
+            assert ccle.save_dose_response_curve(
+                ErbB_expression_ratio,
+                {"value": ["high", "low"]},
+                drug,
+                labels=["EGFR high", "EGFR low"],
+            ) is None
 
-
-def test_activity_area():
-    for drug in ["Erlotinib", "Lapatinib"]:
-        assert ccle.save_activity_area(
-            ErbB_expression_ratio,
-            {"value": ["high", "low"]},
-            drug,
-            labels=["EGFR high", "EGFR low"],
-        ) is None
-    for drug in ["Erlotinib", "Lapatinib"]:
-        assert os.path.isfile(
-            os.path.join("activity_area", "EGFR", f"{drug}.pdf")
-        )
+        for drug in ["Erlotinib", "Lapatinib"]:
+            assert os.path.isfile(
+                os.path.join("dose_response", "EGFR", f"{drug}.pdf")
+            )
 
 
-def test_cleanup():
-    for dirname in ["dose_response", "activity_area"]:
-        if os.path.isdir(dirname):
-            shutil.rmtree(dirname)
+    def test_activity_area():
+        for drug in ["Erlotinib", "Lapatinib"]:
+            assert ccle.save_activity_area(
+                ErbB_expression_ratio,
+                {"value": ["high", "low"]},
+                drug,
+                labels=["EGFR high", "EGFR low"],
+            ) is None
+        for drug in ["Erlotinib", "Lapatinib"]:
+            assert os.path.isfile(
+                os.path.join("activity_area", "EGFR", f"{drug}.pdf")
+            )
+
+
+    def test_cleanup():
+        for dirname in ["dose_response", "activity_area"]:
+            if os.path.isdir(dirname):
+                shutil.rmtree(dirname)
