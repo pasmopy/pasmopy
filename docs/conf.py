8b64249913b8c77c877baebe8b0b0a17fae77691
himoto <31299606+himoto@users.noreply.github.com>
2023-01-16 14:09:36 +0900

    Disable navigation_with_keys

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ pygments_style = "sphinx"
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_theme_options = {"logo_only": True, "navigation_with_keys": True}
+html_theme_options = {"logo_only": True, "navigation_depth": 4}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
